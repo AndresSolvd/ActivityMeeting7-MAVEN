17390e5e3ced8201425eb9bceba6bf47470df5d4
AndresSolvd <agarcianavarro.laba@solvd.com>
2023-05-04 11:49:39 -0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // iterate over the array and replace numbers divisible by 3, 5, or both
+        for (int i = 0; i < 100; i++) {
+            
+            if (i % 3 == 0 && i % 5 ==0) {
+                System.out.println("Banana");
+            } else if (i % 3 == 0) {
+                System.out.println("Apple");
+            } else if (i % 5 == 0) {
+                System.out.println("Orange");                
+            } else {
+                System.out.println(i);
+            }            
+        }
+    }
+}
